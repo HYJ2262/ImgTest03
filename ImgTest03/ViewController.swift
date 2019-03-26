c99a01fbe4602f74f7722824df4018d8b9f048ec
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-03-26 15:35:28 +0900

    Alien Image Moved by button clicked

--- ImgTest03/ViewController.swift
+++ ImgTest03/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    //이미지를 담는 그릇
+    var count = 1
+    var dis = true
+    
+    @IBOutlet weak var myTextLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named:"frame1.png")
+        myTextLable.text = "1"
     }
+    @IBAction func ChangeImage(_ sender: Any) {
+        
+        if count == 5{
+            dis = false
+        }else if count == 1 {
+            dis = true
+        }
+        
+        if dis == true{
+            count += 1
+        }
+        else if dis == false{
+            count -= 1
+        }
 
+        myImageView.image = UIImage(named:"frame\(count).png")
+        
+        myTextLable.text = String(count)
+    }
+    
 
 }
 
